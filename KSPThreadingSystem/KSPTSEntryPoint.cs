7337c5bab1a9b3b942e288bd3d3a75da02f47b14
ferram4 <ferram40@gmail.com>
2014-08-17 21:56:19 -0400

    Initial commit

---
+++ KSPThreadingSystem/KSPTSEntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KSPThreadingSystem
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class KSPTSEntryPoint : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Initializing KSPThreadingSystem");
+            GameObject o = new GameObject();
+
+            o.AddComponent<KSPTSThreadController>();
+
+            GameObject.DontDestroyOnLoad(o);
+        }
+    }
+}
---
+++ KSPThreadingSystem/KSPTSThreadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace KSPThreadingSystem
+{
+    public class KSPTSThreadController : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("KSPTSThreadController Initialized");
+        }
+
+        void Update()
+        {
+            Debug.Log("KSPTSThreadController reporting; scene: " + HighLogic.LoadedScene.ToString());
+        }
+    }
+}
