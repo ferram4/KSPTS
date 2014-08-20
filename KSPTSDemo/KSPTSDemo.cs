31d8f44c09a5ccd142daee759ee05e1584a60e3e
ferram4 <ferram40@gmail.com>
2014-08-20 09:25:23 -0400

    More demo stuff

--- KSPTSDemo/KSPTSDemo.cs
+++ KSPTSDemo/KSPTSDemo.cs
@@ -8,7 +8,8 @@ namespace KSPTSDemo
     [KSPAddon(KSPAddon.Startup.MainMenu, false)]
     public class KSPTSDemoController : MonoBehaviour
     {
-        KSPTSDemoObject[] array;
+        private Rect windowRect = new Rect(100, 100, 300, 300);
+        List<KSPTSDemoObject> list;
 
         const int c = 4;
         readonly object locker = new object();
@@ -16,37 +17,59 @@ namespace KSPTSDemo
         void Start()
         {
             Debug.Log("Starting KSPTS Demo");
-            array = new KSPTSDemoObject[c];
+            list = new List<KSPTSDemoObject>();
             for (int i = 0; i < c; i++)
-                array[i] = new KSPTSDemoObject(i);
+                list.Add(new KSPTSDemoObject(i));
+
+            this.enabled = true;
         }
 
-        /*void Update()
+        public void OnGUI()
         {
-            string s = "";
-            for (int i = 0; i < c; i++)
-                s += "Index " + i + ": " + array[i];
+            windowRect = GUI.Window(this.GetHashCode(), windowRect, ExceptionThrowerGUI, "KSPTS Demo");
+        }
 
-            Debug.Log(s);
-        }*/
+        private void ExceptionThrowerGUI(int id)
+        {
+            GUILayout.Label("Use this to add and remove tasks from OnUpdate");
+            if (GUILayout.Button("Add New Task"))
+            {
+                list.Add(new KSPTSDemoObject(list.Count));
+            }
+            if (GUILayout.Button("Remove Last Task"))
+            {
+                KSPTSDemoObject tmp = list[list.Count - 1];
+                list.Remove(tmp);
+                tmp.UnRegister();
+            }
+            string s = "Output:\n\r";
+            for (int i = 0; i < list.Count; i++)
+            {
+                s += (i + " " + list[i].output + "\n\r");
+            }
+            GUILayout.Label(s);
+            GUI.DragWindow();
+        }
+
     }
 
     public class KSPTSDemoObject
     {
         int val;
-        //System.Random rand;
+        public string output = "";
+        System.Random rand;
 
         public KSPTSDemoObject(int val)
         {
             this.val = val;
-            //rand = new System.Random(val * System.DateTime.Now.Millisecond);
+            rand = new System.Random(val * System.DateTime.Now.Millisecond);
 
             KSPTSAPI.RegisterNewThreadTask(KSPTSThreadingGroup.IN_LOOP_UPDATE, PreFunction, ThreadedTask, PostFunction);
         }
 
         public object PreFunction()
         {
-            Debug.Log("Running PreFunction");
+            //Debug.Log("Running PreFunction");
             return val;
         }
 
@@ -54,14 +77,20 @@ namespace KSPTSDemo
         {
             int value = (int)parameter;
 
-            int output = 25 * value;
+            int output = rand.Next(25) * value;
 
             return output;
         }
 
         public void PostFunction(object parameter)
         {
-            Debug.Log("Running PostFunction; output of threadedTask is: " + parameter);
+            //Debug.Log("Running PostFunction");
+            output = parameter.ToString();
+        }
+
+        public bool UnRegister()
+        {
+            return KSPTSAPI.TryUnregisterThreadTask(KSPTSThreadingGroup.IN_LOOP_UPDATE, ThreadedTask);
         }
     }
 }
